2c0cc416726b4a5e1314004bdae642d54a1366df
NongFirstTH <TanabodeeChannel@gmail.com>
2023-01-14 18:34:41 +0700

    OK

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,33 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double num[],int N,double B[]){
+    double sum,x,y,z,min=num[0],max=num[0];
+    for(int i = 0;i<N;i++){
+        sum = B[0] += num[i];
+    }
+    B[0] /=N;
+
+    for(int i =0;i<N;i++){
+        x += pow(num[i]-(sum/N),2);     
+    }
+    B[1] = sqrt(x/N);
+
+    for(int i =0;i<N;i++){
+       y = B[2] += log10(num[i]); 
+    }
+    B[2] = pow(10,y/N);
+
+    for(int i =0;i<N;i++){
+       z = B[3] += 1/num[i]; 
+    } 
+    B[3] = N/z;
+    
+    for(int i =1;i<N;i++){
+       if(num[i]>max) max=num[i];
+       if(num[i]<min) min=num[i]; 
+    }
+    B[4] = max;
+    B[5] = min;
+}
